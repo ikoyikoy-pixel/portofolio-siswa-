ce317c28b8d6d5331676ce91f1f85aa5a240c9ba
ikoyikoy-pixel <mrikonurfadilah010@gmail.com>
2026-01-30 11:34:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function login() {
+  document.querySelector('.login-container').style.display = 'none';
+  document.querySelector('.portfolio').classList.remove('hidden');
+}
+
+function hubungi() {
+  alert("Terima kasih sudah mengunjungi portofolio saya 🙏");
+}
+
+function toggleTheme() {
+  document.body.classList.toggle('dark');
+}
+
